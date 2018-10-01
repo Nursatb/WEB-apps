8a3dd3071df48b7e37984ac98bfd69ba2beb7e0e
Nursat <Nursatb@icloud.com>
2018-10-01 19:14:34 +0300

    LAB 1
    bugs was fixed

--- PIP1/LS1.js
+++ PIP1/LS1.js
@@ -1,22 +1,84 @@
-function Durakfunc(){
-	var validate = false;
-	xnow = document.getElementById("Xvalue").value;
-	if (xnow< -3 || xnow>3) {
-		window.alert("Значения не правильны!!!");
-	} 
-	else {
-		validate = true;
+var x,y,r;
+
+function dot(xnow) {
+	xnow = xnow.replace(/,/, '.');
+	return xnow;
+}
+
+function incBtn(b) {
+  document.getElementById('rin').value = b;
+}
+
+function Durakfunc(f){
+f.Xvalue.value = f.Xvalue.value.replace(',','.');
+var xnow = f.Xvalue.value;
+	if(isNaN(parseFloat(xnow)) || isNaN(xnow) || !isFinite(xnow) || xnow < -3 || xnow > 3){
+	  	window.alert("Исправьте значения X");
+	    return false;
 	}
+}
+
+// function params(f) {
+// 	var validate = false;
+// 	xnow = document.getElementById("Xvalue").value.replace(/,/ , '.');
+// 	xnow = parseFloat(xnow);
+// 	ynow = parseFloat(document.getElementById("Yvalue").value);
+// 	rnow = parseFloat(document.getElementById("R").value);
+// 	if ( isNaN(ynow) || isNaN(xnow) || isNaN(rnow)) {
+// 		window.alert("write param's")
+// 		return false;
+// 	}
+// }
+
+
+
+
+	// var validate = false;
+	// xnow = document.getElementById("Xvalue").value.replace(/,/ , '.');
+	// xnow = parseFloat(xnow);
+	// ynow = parseFloat(document.getElementById("Yvalue").value);
+	// rnow = parseFloat(document.getElementById("R").value);
+	// console.log(xnow);
+	// var xtf = isNaN(parseFloat(ynow));
+	// console.log(xtf);
+	// if ( !isNaN(ynow) || !isNaN(xnow) || !isNaN(rnow) || !isFinite(xnow)) {
+	// 	window.alert("write param's")
+	// 	return false;
+	// } else if (xnow <= -3 || xnow >= 3) {
+	// 	window.alert("nooooo");
+	// } else {
+	// 	validate = true;
+	// }
+
+	// if (validate == false) {
+	// 	window.alert("вводите норм");
+	// }
+
+
+	// if (ynow == null || xnow==null || rnow==null) {
+	// 	window.alert("Ввдедите значения");
+	// }
+
+	
+
+	// if (input.value != input.value.replace(/[^\d,]/g, '')) {
+	// 	window.alert("")
+	// }
 
 	// else if (isNaN(xnow)){
 	// 	window.alert("Значения не правильны");
 	// } else {
 	// 	window.alert("wright!")
 	// };
-	return validate;
 
 
+	// return validate;
 
 
-}
+//при пустом вводе выводить ошибку, запятые,
+// function getFloat(){
+//   return parseFloat(document.getElementById("Xvalue").value .replace(/,/, '.'));
+// }
+
+// (/[^\d,.]/g, '')
 
--- PIP1/actions.php
+++ PIP1/actions.php
@@ -1,51 +1,67 @@
 <?php
 	session_start();
+	date_default_timezone_set("UTC");
+	date_default_timezone_set('Europe/Moscow');
 	$tm = microtime(true);
 	$ts = date('m/d/Y h:i:s a');
-	$X = $_GET["Xvalue"];
-	$Y = $_GET["Yvalue"];
-	$R = $_GET["R"];
+	$X = $_REQUEST["Xvalue"];
+	$Y = $_REQUEST["Yvalue"];
+	$R = $_REQUEST["R"];
 	$tr = false;
-	$right = is_numeric($X) && is_numeric($Y) && is_numeric($R) &&
-	 in_array($Y, array("-5","-4","-3","-2","-1","0","1","2","3")) && floatval($X) >=-3 && floatval($X) <= 3 &&
+	$right = is_numeric($X) && is_numeric($Y) && is_numeric($R) && floatval($X) >=(-3) && floatval($X)<= 3 && in_array($Y, array("-5","-4","-3","-2","-1","0","1","2","3"))  &&
 	in_array($R, array("1", "1.5","2", "2.5", "3"));
+
 ?>
 <!DOCTYPE html>
 <html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<style type="text/css">
+		table {
+		    border-collapse: collapse;
+		    width: 100%;
+		}
+
+		th, td {
+		    text-align: left;
+		    padding: 8px;
+		}
+
+		tr:nth-child(even){background-color: #f2f2f2}
+
+		th {
+		    background-color: rgb(95,151,252);
+		    color: white;
+		}
+    </style>
 
 </head>
 <body>
 	<table border="1px">
 		<tr>
-	        <td>
+	        <th>
 	          X
-	        </td>
-	        <td>
+	        </th>
+	        <th>
 	          Y
-	        </td>
-	        <td>
+	        </th>
+	        <th>
 	          R
-	        </td>
-	        <td>
+	        </th>
+	        <th>
 	          Result
-	        </td>
-	        <td>
+	        </th>
+	        <th>
 	          Current time
-	        </td>
-	        <td>
+	        </th>
+	        <th>
 	          Execution time
-	        </td>
-
-	        <td>
-	        	TR
-	        </td>
+	        </th>
       </tr>
 
 
       <?php
-       if ($right) {
+       
         $X = (double)$_REQUEST['Xvalue'];
         $Y = (double)$_REQUEST['Yvalue'];
         $R = (double)$_REQUEST['R'];
@@ -54,11 +70,11 @@
         $_SESSION['X'.$len] = $X;
         $_SESSION['Y'.$len] = $Y;
         $_SESSION['R'.$len] = $R;
-		if(($R*$R >=  $X*$X + $Y*$Y)  && ($Y >= 0 && $X <= 0)){ //circle
+		if($Y >= 0 && $X <= 0 && $R*$R >=  $X*$X + $Y*$Y){
 			$tr = true;
-		} else if (($R >= $X && $R/2 >= $Y) && ($X >= 0 && $Y >= 0)){ //прямоуг
+		} else if ($X >= 0 && $Y >= 0 && $X <= $R && $Y <= $R/2){
 			$tr=true;
-		} else if ( ($Y >= -($X/2 + $R)) && ($X <= 0 && $Y <= 0)) {
+		} else if ($Y = -(2*$X + $R) && $X <= 0 && $Y <= 0) {
 			$tr = true;
 		}
         else $tr = false;
@@ -66,7 +82,7 @@
         $rt = number_format(1000 * (microtime(true)-$tm),5) . ' msec';
         $_SESSION['ts' . $len] = $ts;
         $_SESSION['rt' . $len] = $rt;
-    	}
+    	
       for($i = $len; $i > 0; $i--){
         $X = $_SESSION['X'.$i];
         $Y = $_SESSION['Y'.$i];
@@ -74,6 +90,7 @@
         $tr = $_SESSION['tr'.$i];
         $ts = $_SESSION['ts'.$i];
         $rt = $_SESSION['rt'.$i];
+    	
       ?>
 
 
@@ -95,9 +112,9 @@
 
 			<td>
 				<?php
-					if ($tr == true ) {
-						echo "попал";
-					} else { echo "не попал";}
+					if ($tr) 
+					echo "попал";
+					else  echo "не попал";
 				 ?>
 			</td>
 
@@ -110,14 +127,6 @@
 				<?php
 				echo $rt; ?>
 			</td>
-
-			<td>
-				<?php
-					echo $tr;
-				 ?>
-			</td>
-
-
 		</tr>
 	<?php } ?>
 	</table>
